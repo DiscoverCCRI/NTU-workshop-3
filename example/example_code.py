37c6308b42584d24b421357c3612e17648982a95
cjb873 <cjb873@nau.edu>
2023-08-08 13:40:25 -0700

    Reorganize example

---
+++ example/example_code.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+from rover_api.discover_tracking import Tracker
+from rover_api.discover_camera import Camera
+
+TOTAL_MARKERS = 5
+
+
+def add_marker(markers, marker_id, marker_distance, camera):
+
+    if marker_id not in markers and marker_id >= 0:
+        markers.append(marker_id)
+        camera.get_jpg()        
+        log_marker(marker_id, marker_distance, len(markers))
+
+
+def log_marker(marker_id, marker_distance, num_markers):
+    with open("/experiment/catalog.md", "a+", encoding="utf-8") as outfile:
+        print(f"logging marker: {marker_id}")
+        outfile.write(f"The marker's id is {marker_id}. ")
+        outfile.write(f"The marker was {marker_distance} from the robot.\n")
+        outfile.write(f"The image is: ![{marker_id}](photos/{num_markers - 1}.jpg)\n\n")
+        
+
+def main():
+
+    tracker = Tracker()
+    camera = Camera()
+    markers = []
+
+    while TOTAL_MARKERS > len(markers):
+        while not tracker.get_marker_visible():
+            pass
+        
+        marker_distance = tracker.get_marker_distance()
+        marker_id = tracker.get_marker_id()
+        if (marker_distance >= 1.5 and marker_distance <= 2):
+            add_marker(markers, marker_id, marker_distance, camera)
+            
+
+if __name__ == "__main__":
+    main()
---
+++ example/experiment/catalog.md
@@ -0,0 +1,15 @@
+The marker's id is 4. The marker was 1.5371970383024098 from the robot.
+The image is: ![4](photos/0.jpg)
+
+The marker's id is 9. The marker was 1.9821262235791124 from the robot.
+The image is: ![9](photos/1.jpg)
+
+The marker's id is 11. The marker was 1.9606229940505164 from the robot.
+The image is: ![11](photos/2.jpg)
+
+The marker's id is 7. The marker was 1.9728081982885073 from the robot.
+The image is: ![7](photos/3.jpg)
+
+The marker's id is 15. The marker was 1.5219367371046826 from the robot.
+The image is: ![15](photos/4.jpg)
+
